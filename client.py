45702b2022adf152e8dd06bdab510ddb8862bd8a
Moussa_Haidar <moussahaidar999@gmail.com>
2024-07-07 16:10:50 +0300

    Updated client.py, adding check_run_localhost method

--- client.py
+++ client.py
@@ -1,15 +1,20 @@
-# frontend-repo/client.py
+# frontend/client.py
 
 import yaml
 
 class Client:
-    def __init__(self, config_file):
+    def __init__(self, config_file='../config.yaml'):
         with open(config_file, 'r') as file:
             self.config = yaml.safe_load(file)
     
     def print_server_ip(self):
         print(f"Server IP Address: {self.config['ServerIPAddress']}")
 
+    def check_run_localhost(self):
+        if self.config.get('RunLocalhost', False):
+            print("Running on localhost. Please update configurations if necessary.")
+
 if __name__ == "__main__":
-    client = Client('../config.yaml')
+    client = Client()
     client.print_server_ip()
+    client.check_run_localhost()
